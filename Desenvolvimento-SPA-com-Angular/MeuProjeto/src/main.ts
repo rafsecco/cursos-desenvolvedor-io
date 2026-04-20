153da26a3c63a2886a9b5a8851dcc6e55eb3bef0
Rafael Secco <raf.secco@gmail.com>
2026-04-20 00:14:31 -0300

    feat: add locale configuration for Brazilian Portuguese support

--- Desenvolvimento-SPA-com-Angular/MeuProjeto/src/main.ts
+++ Desenvolvimento-SPA-com-Angular/MeuProjeto/src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'; // Use 'pt' instead of 'pt-br'
+
+registerLocaleData(localePt);
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
